8f9b165139da8f9bb5ad03675af8eaa16d50e23f
Nicholas Tapia <tapzx2@Nicholass-MacBook-Air.local>
2022-02-11 22:58:23 -0500

    add freq distribution of letters but not loving effect

--- sudo1.js
+++ sudo1.js
@@ -23,21 +23,76 @@
 // lengths = word length Range: 3 to grid max length
 
 
+//frequency distribution
+//https://www3.nd.edu/~busiforc/handouts/cryptography/letterfrequencies.html
+
+var distLetters = ""
+// E	11.1607% 56.88
+distLetters += "e".repeat(5688)
+// M	3.0129%	15.36
+distLetters += "m".repeat(1536)
+// A	8.4966%	43.31
+distLetters += "a".repeat(4331)
+// H	3.0034%	15.31
+distLetters += "h".repeat(1531)
+// R	7.5809%	38.64
+distLetters += "r".repeat(3864)
+// G	2.4705%	12.59
+distLetters += "g".repeat(1259)
+// I	7.5448%	38.45
+distLetters += "i".repeat(3845)
+// B	2.0720%	10.56
+distLetters += "g".repeat(1056)
+// O	7.1635%	36.51
+distLetters += "o".repeat(3651)
+// F	1.8121%	9.24
+distLetters += "f".repeat(924)
+// T	6.9509%	35.43
+distLetters += "t".repeat(3543)
+// Y	1.7779%	9.06
+distLetters += "y".repeat(906)
+// N	6.6544%	33.92
+distLetters += "n".repeat(3392)
+// W	1.2899%	6.57
+distLetters += "w".repeat(657)
+// S	5.7351%	29.23
+distLetters += "s".repeat(2923)
+// K	1.1016%	5.61
+distLetters += "k".repeat(561)
+// L	5.4893%	27.98
+distLetters += "l".repeat(2798)
+// V	1.0074%	5.13
+distLetters += "v".repeat(513)
+// C	4.5388%	23.13
+distLetters += "c".repeat(2313)
+// X	0.2902%	1.48
+distLetters += "x".repeat(148)
+// U	3.6308%	18.51
+distLetters += "u".repeat(1851)
+// Z	0.2722%	1.39
+distLetters += "z".repeat(139)
+// D	3.3844%	17.25
+distLetters += "d".repeat(1725)
+// J	0.1965%	1.00
+distLetters += "j".repeat(100)
+// P	3.1671%	16.14
+distLetters += "p".repeat(1614)
+// Q	0.1962%	(1)
+distLetters += "q".repeat(100)
+
 
 // create model
 console.log('it works')
 const gridSize = 15
-const letters = ['a','b','c','d','e','f','g','j','i','j','k','l','m','n','o','p','q','r','s','t','v','u','w','x','y','z']
+//const letters = ['a','b','c','d','e','f','g','j','i','j','k','l','m','n','o','p','q','r','s','t','v','u','w','x','y','z']
 const grid = []
 for (let i=0;i<gridSize;i++){
     var row = []
     for (let j=0;j<gridSize;j++){
-        row.push(letters[getRandomInt(letters.length)])
+        row.push(distLetters[getRandomInt(distLetters.length)])
     }
     grid.push(row)
 }
-console.log(letters.length)
-console.log(grid)
 
   // get random letter helpers
 function getRandomInt(max) {
